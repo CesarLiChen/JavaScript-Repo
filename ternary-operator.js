899ae23fbbe4d064c19ee7f77b6e4b208a6c8422
Cesar Li (win10) <cesarli619@gmail.com>
2022-12-06 16:51:33 -0500

    The ternary operator, looks the same as Java

---
+++ ternary-operator.js
@@ -0,0 +1,13 @@
+// Ternary operator
+// Shortcut for if-else
+
+let dollars = 1;
+
+// (cond) ? (value if true) : (value if false)
+console.log(dollars + (dollars > 1 ? " dollars" : " dollar"));
+
+// if (dollars > 1) {
+// 	console.log(dollars + " dollars");
+// } else {
+// 	console.log(dollars + " dollar");
+// }
