0fd3df8599d31fb56e45c950a2eac555439d4188
Cesar Li (win10) <cesarli619@gmail.com>
2022-12-06 18:06:08 -0500

    While and Do-while loops in javascript

---
+++ do-while-loops.js
@@ -0,0 +1,16 @@
+// Do While Loops
+// Evaluate cond AFTER statement is ran
+
+let cond2 = 1;
+do {
+	console.log("Do do do " + cond2);
+	cond2 += 1;
+} while (cond2 <= 5);
+
+console.log("End loop");
+
+/* 
+Something learned, can't have same variable
+name in two different Javascript files
+in the same directory.
+*/
---
+++ while-loops.js
@@ -0,0 +1,11 @@
+// While loops
+
+console.log("Before loop =====================")
+
+let cond = 1;
+while (cond <= 5) {
+	console.log("Ahh" + cond);
+	cond +=  1;
+}
+
+console.log("After loop =====================")
